794dda4448279b2970da2fa89f7fcd83b5fff7cf
Youssef Ghareb <youssef.ghareb99@gmail.com>
2022-11-27 20:24:33 +0200

    Design changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,11 @@
 import Button from "./Button"
+import { AiOutlineCloseSquare } from 'react-icons/ai'
 
 const Header = ({ title, click, showAdd }) => {
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button bg={showAdd ? "red" : "green"} text={showAdd ? "Close" : "Show"} click={click} />
+            <Button bg={showAdd ? "white" : "green"} text={showAdd ? <AiOutlineCloseSquare className="close-icon" /> : "Show"} click={click} />
         </header>
     )
 }
